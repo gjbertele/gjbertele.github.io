2cbd940c4389b8bff8caee2aeb7b8b5b9113d7b5
gjbertele <58128243+gjbertele@users.noreply.github.com>
2025-06-08 15:50:05 -0400

    a

--- n-wordle.js
+++ n-wordle.js
@@ -108,7 +108,6 @@ document.querySelector('.submit').onclick = function(e){
         document.querySelector('.submit').textContent = 'GUESS';
         elem.value = '';
         elem.placeholder = 'Type your guess';
-        elem.inputMode = 'text';
     } else {
         if(elem.value.length != 5){
             elem.value = "Guess must be 5 letters!";
