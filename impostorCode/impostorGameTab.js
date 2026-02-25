0f683345307fa4cf6ecee3749cf70585012975e9
gjbertele <58128243+gjbertele@users.noreply.github.com>
2026-02-25 15:29:49 -0500

    added custom word impostor

---
+++ impostorCode/impostorGameTab.js
@@ -0,0 +1,168 @@
+const playerStartElement = document.querySelector('.gamePage > .playerStart');
+const playerGrid = document.querySelector('.gamePage > .playerGrid');
+const backButtons = document.querySelectorAll('.backButton');
+const displayTextArea = document.querySelector('.textDisplay');
+
+let chosenWord = '';
+let chosenCategory = '';
+
+let chosenImpostors = [];
+let impostorDictionary = [{
+    word:'a',
+    category:'category a'
+}];
+
+
+
+const startGame = () => {
+    document.querySelector('.settingsPage').style.display = 'none';
+    document.querySelector('.gamePage').style.display = 'inline-block';
+
+    chooseWord();
+    chooseImpostors();
+    choosePlayerStart();
+
+    playerGrid.innerHTML = '';
+    for(let i = 0; i<players.length; i++) addPlayerToGrid(players[i], i);
+
+    return;
+}
+
+const addPlayerToGrid = (player, i) => {
+    const newPlayerElement = document.createElement('div');
+    newPlayerElement.className = 'card gridCard';
+    
+    const playerNameElement = document.createElement('span');
+    playerNameElement.textContent = player.name;
+    playerNameElement.className = 'playerName';
+
+    const playerProfilePicture = document.createElement('div');
+    playerProfilePicture.textContent = player.name.charAt(0);
+    playerProfilePicture.className = 'playerProfile';
+
+    newPlayerElement.appendChild(playerNameElement);
+    newPlayerElement.appendChild(playerProfilePicture);
+
+    playerGrid.appendChild(newPlayerElement);
+
+    if(i % 2 == 0) newPlayerElement.style.left = '10%';
+    else newPlayerElement.style.right = '10%';
+
+    newPlayerElement.style.top = (Math.floor(i/2)*42.5)+'vw';
+
+    playerGrid.style.height = ((Math.floor(i/2)+1)*42.5)+'vw';
+
+    newPlayerElement.onclick = () => {
+        displayPlayer(player, newPlayerElement);
+
+        return;
+    };
+
+
+    return;
+}
+
+const chooseWord = () => {
+    if(words.length > 0 && Math.random() < 0.4){
+        let randomIdx = Math.floor(Math.random()*words.length);
+        chosenWord = words[randomIdx].word;
+        chosenCategory = 'Custom Word';
+    } else {
+        let randomIdx = Math.floor(Math.random()*impostorDictionary.length);
+        chosenWord = impostorDictionary[randomIdx].word;
+        chosenCategory = impostorDictionary[randomIdx].category;
+    }
+
+    return;
+}
+
+const choosePlayerStart = () => {
+    let randomIdx = Math.floor(Math.random()*players.length);
+    playerStartElement.textContent = players[randomIdx].name+" Starts";
+
+    return;
+}
+
+const chooseImpostors = () => {
+    let playersClone = [];
+    for(let i = 0; i<players.length; i++){
+        players[i].isImpostor = false;
+        playersClone.push(players[i]);
+    }
+
+    for(let i = 0; i<Math.min(impostorCount,playersClone.length); i++){
+        let randomIdx = Math.floor(Math.random()*playersClone.length);
+        chosenImpostors.push(playersClone[randomIdx]);
+        playersClone.splice(randomIdx, 1);
+    }
+
+    for(let i = 0; i<chosenImpostors.length; i++){
+        for(let j = 0; j<players.length; j++){
+            if(chosenImpostors[i].id == players[j].id){
+                players[j].isImpostor = true;
+            }
+        }
+    }
+
+    return;
+}
+
+const displayPlayer = (player, element) => {
+    element.style.filter = 'brightness(75%)';
+    element.style.boxShadow = 'none';
+    element.onclick = null;
+    
+    if(!player.isImpostor){
+        displayTextArea.innerHTML = `
+        <span class="default">The word for </span>
+        <span class="highlighted">${player.name}</span>
+        <span class="default"> is </span>
+        <span class="highlighted">${chosenWord}</span> 
+        <span class="default"> and the category is </span>
+        <span class="highlighted">${chosenCategory}</span>
+        `
+    } else {
+        displayTextArea.innerHTML = `
+        <span class="highlighted">${player.name}</span>
+        <span class="default"> is an </span>
+        <span class="highlightedRed">Impostor</span> 
+        <span class="default"> and the category is </span>
+        <span class="highlighted">${chosenCategory}</span>
+        `
+    }
+
+    document.querySelector('.gamePage').style.display = 'none';
+    document.querySelector('.playerDisplay').style.display = 'inline-block';
+}
+
+
+let lastBackTime = 0;
+
+document.querySelector('.playerDisplay > .backButton').addEventListener('click', () => {
+    if(Date.now() - lastBackTime < 1000) return;
+    lastBackTime = Date.now();
+
+    document.querySelector('.gamePage').style.display = 'inline-block';
+    document.querySelector('.playerDisplay').style.display = 'none';
+});
+
+document.querySelector('.gamePage > .backButton').addEventListener('click', () => {
+    if(Date.now() - lastBackTime < 1000) return;
+    lastBackTime = Date.now();
+
+    document.querySelector('.settingsPage').style.display = 'inline-block';
+    document.querySelector('.gamePage').style.display = 'none';
+});
+
+
+
+let backSize = document.body.clientWidth*0.08;
+const backButtonSVG = `<svg width="${backSize}" height="${backSize}" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_6_13443)">
+<path d="M11.67 3.86998L9.9 2.09998L0 12L9.9 21.9L11.67 20.13L3.54 12L11.67 3.86998Z" fill="#FFF"/>
+</g>
+</svg>
+`
+
+backButtons[0].innerHTML = backButtonSVG;
+backButtons[1].innerHTML = backButtonSVG;
---
+++ impostorCode/impostorSettingsTab.js
@@ -0,0 +1,186 @@
+const removeSVG = `<svg class="removeButtonSVG" width="${document.body.clientHeight*0.04}" height="${document.body.clientHeight*0.04}" viewBox="0 0 118 118" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M78.6666 59H39.3333" stroke="#FFF" stroke-width="8.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12.2917 59C12.2917 36.9815 12.2917 25.9723 19.1319 19.1319C25.9723 12.2917 36.9815 12.2917 59 12.2917C81.0183 12.2917 92.0277 12.2917 98.8683 19.1319C105.708 25.9723 105.708 36.9815 105.708 59C105.708 81.0183 105.708 92.0277 98.8683 98.8683C92.0277 105.708 81.0183 105.708 59 105.708C36.9815 105.708 25.9723 105.708 19.1319 98.8683C12.2917 92.0277 12.2917 81.0183 12.2917 59Z" stroke="#FFF" stroke-width="8.5"/>
+</svg>`;
+
+const addPlayerButton = document.querySelector('.addPlayer');
+const playerElementHolder = document.querySelector('.playerHolder');
+const afterPlayerHolder = document.querySelector('.afterPlayerHolder');
+const impostorCountInput = document.querySelector('.impostorCount > .editableLabel');
+const wordElementHolder = document.querySelector('.wordHolder');
+const afterWordHolder = document.querySelector('.afterWordHolder');
+const addWordButton = document.querySelector('.addWord');
+const startGameButton = document.querySelector('.startGame');
+
+impostorCountInput.value = 1;
+
+let afterPlayerHolderTop = 30;
+let afterWordHolderTop = 30;
+
+let players = [];
+let words = [];
+let impostorCount = 1;
+
+const addPlayer = () => {
+    let newPlayerElement = document.createElement('div');
+    newPlayerElement.className = 'card playerCard';
+
+    let playerName = document.createElement('input');
+    playerName.type = 'text';
+    playerName.className = 'editableLabel'
+    playerName.value = 'Player '+(players.length + 1);
+
+    let removeButton = document.createElement('div');
+    let svgRemoveChild = document.createElement('svg');
+    removeButton.appendChild(svgRemoveChild);
+    svgRemoveChild.outerHTML = removeSVG;
+    removeButton.className = 'removeButton';
+
+
+    let playerID = Math.floor(Math.random()*1000000);
+    newPlayerElement.id = playerID;
+
+    newPlayerElement.appendChild(playerName);
+    newPlayerElement.appendChild(removeButton);
+
+
+    playerElementHolder.appendChild(newPlayerElement);
+
+
+    newPlayerElement.style.top = (players.length * 10)+"vh";
+
+    afterPlayerHolderTop+=10;
+    afterPlayerHolder.style.top = afterPlayerHolderTop+'vh';
+
+
+    players.push({
+        name: 'Player '+(players.length + 1),
+        isImpostor:null,
+        id: playerID
+    });
+
+
+    playerName.onchange = () => {
+        for(let i = 0; i<players.length; i++){
+            if(players[i].id != playerID) continue;
+            players[i].name = playerName.value;
+        }
+        
+        return;
+    }
+
+    removeButton.addEventListener('click',() => {
+        afterPlayerHolderTop -= 10;
+        afterPlayerHolder.style.top = afterPlayerHolderTop+'vh';
+
+        for(let i = 0; i<players.length; i++){
+            if(players[i].id == playerID) players.splice(i, 1);
+        }
+
+        for(let i in playerElementHolder.childNodes){
+            let elem = playerElementHolder.childNodes[i];
+            if(elem.id == playerID) playerElementHolder.removeChild(elem);
+        }
+
+        for(let i in playerElementHolder.childNodes){
+            let elem = playerElementHolder.childNodes[i];
+            if(elem.style) elem.style.top = (i * 10) + "vh";
+        }
+        
+        return;
+    });
+
+    return;
+}
+
+const addWord = (wordValue = '') => {
+    let newWordElement = document.createElement('div');
+    newWordElement.className = 'card wordCard';
+
+    let wordContent = document.createElement('input');
+    wordContent.type = 'text';
+    wordContent.className = 'editableLabel'
+    wordContent.placeholder = "Type Here";
+    if(typeof wordValue == 'string') wordContent.value = wordValue;
+
+    let removeButton = document.createElement('div');
+    removeButton.className = 'removeButton';
+    let svgRemoveChild = document.createElement('svg');
+    removeButton.appendChild(svgRemoveChild);
+    svgRemoveChild.outerHTML = removeSVG;
+
+    newWordElement.appendChild(wordContent);
+    newWordElement.appendChild(removeButton);
+
+    wordElementHolder.appendChild(newWordElement);
+
+    newWordElement.style.top = (words.length * 10)+"vh";
+
+    afterWordHolderTop+=10;
+    afterWordHolder.style.top = afterWordHolderTop+'vh';
+
+
+    let wordID = Math.floor(Math.random()*1000000);
+    newWordElement.id = wordID;
+
+    words.push({
+        word: wordValue,
+        id: wordID
+    });
+
+
+    wordContent.onchange = () => {
+        for(let i = 0; i<players.length; i++){
+            if(words[i].id == wordID) words[i].word = wordContent.value;
+        }
+        return;
+    }
+
+    removeButton.addEventListener('click',() => {
+        afterWordHolderTop -= 10;
+        afterWordHolder.style.top = afterWordHolderTop+'vh';
+
+        for(let i = 0; i<words.length; i++){
+            if(words[i].id == wordID) words.splice(i, 1);
+        }
+
+        for(let i in wordElementHolder.childNodes){
+            let elem = wordElementHolder.childNodes[i];
+            if(elem.id == wordID) wordElementHolder.removeChild(elem);
+        }
+
+        for(let i in wordElementHolder.childNodes){
+            let elem = wordElementHolder.childNodes[i];
+            if(elem.style) elem.style.top = (i * 10) + "vh";
+        }
+        
+        return;
+    });
+
+    return;
+}
+
+impostorCountInput.onchange = () => {
+    impostorCount = impostorCountInput.value;
+}
+
+
+addPlayerButton.onclick = addPlayer;
+addPlayer();
+
+addWordButton.onclick = addWord;
+startGameButton.onclick = startGame;
+
+
+addPlayer();
+addPlayer();
+addWord('test 1');
+addWord('test 2');
+addWord('test 3');
+startGame();
+
+
+
+
+
+
