84d4acb2f587a0833c67b0fcc3f02d7c6176670a
gjbertele <58128243+gjbertele@users.noreply.github.com>
2026-03-02 20:10:09 -0500

    fix laptop force

--- impostorCode/impostorMain.js
+++ impostorCode/impostorMain.js
@@ -1,6 +1,6 @@
 const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-//if(!window.location.href.includes('impostorLaptop') && !isIOS) window.location.replace('https://math.gb.net/impostorLaptop');
+if(!window.location.href.includes('impostorLaptop') && !isIOS) window.location.replace('https://math.gb.net/impostorLaptop');
 
 document.querySelector('.gamePage').style.display = 'none';
 document.querySelector('.playerDisplay').style.display = 'none';
