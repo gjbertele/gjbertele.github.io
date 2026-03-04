5d3c5ccf87ad48d6e183e2eda3acf5a7a17e372e
gjbertele <58128243+gjbertele@users.noreply.github.com>
2026-03-03 20:22:54 -0500

    add pushstates

--- impostorCode/impostorMain.js
+++ impostorCode/impostorMain.js
@@ -1,6 +1,12 @@
 const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-if(!window.location.href.includes('impostorLaptop') && !isIOS) window.location.replace('https://math.gb.net/impostorLaptop');
+if(!isIOS){
+    if(!window.location.href.includes('impostorLaptop')){
+        window.location.replace('./impostorLaptop.html');
+    } else {
+        window.history.pushState({},'','./impostor');
+    }
+}
 
 document.querySelector('.gamePage').style.display = 'none';
 document.querySelector('.playerDisplay').style.display = 'none';
