35c0e53f3a5ed77a164d271400cf348f06d19be4
gjbertele <58128243+gjbertele@users.noreply.github.com>
2026-03-25 12:53:00 -0400

    push movement

---
+++ movementCode/movement.js
@@ -0,0 +1,43 @@
+let user = {
+    socket: null
+}
+
+document.body.onclick = () => {
+    DeviceMotionEvent.requestPermission().then((state) => {
+        document.write(state);
+        if (state != 'granted') return;
+
+        window.addEventListener('devicemotion', (e) => {
+            user.socket.send(JSON.stringify({
+                timeSent: Date.now(),
+                acceleration: e.acceleration
+            }));
+        });
+    });
+}
+
+
+const connectToWebSocket = () => {
+    user.socket = new WebSocket("ws://gjb.one");
+
+    user.socket.addEventListener("open", () => {
+        console.log("Connected to server");
+        document.write('connected');
+    });
+
+    user.socket.addEventListener("message", (event) => {
+        console.log("Message from server:", event.data);
+    });
+
+    user.socket.addEventListener("close", () => {
+        console.log("Disconnected");
+        document.write('disconnected');
+    });
+
+    user.socket.addEventListener("error", (err) => {
+        console.error("WebSocket error:", err);
+        document.write(err);
+    });
+}
+
+connectToWebSocket();
