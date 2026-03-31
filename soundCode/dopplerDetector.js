8564c828439dd27c49fbcd58dff9edb7245bfa1e
gjbertele <58128243+gjbertele@users.noreply.github.com>
2026-03-31 13:59:29 -0400

    update scroll

--- soundCode/dopplerDetector.js
+++ soundCode/dopplerDetector.js
@@ -1,40 +1,58 @@
+const speedOfSound = 343.14;
+
 class WorkletProcessor extends AudioWorkletProcessor {
     constructor(){
         super();
         this.totalSamples = 0;
         this.sampleRate = 48000;
-        this.frequencyRange = [19900, 20100];
-        this.frequencySize = this.frequencyRange[1] - this.frequencyRange[0] + 1;
+        this.frequencyTargets = [20000];
+        this.frequencyBand = 3000;
     }
 
     process(inputs){
         inputs = inputs[0][0];
 
         this.totalSamples += inputs.length;
-        let maxAmplitude = 0;
-        let maxFreq = -1;
 
-        for(let i = 0; i<this.frequencySize; i++){
-            let sinSum = 0;
-            let cosSum = 0;
-            let f = i + this.frequencyRange[0];
+        let frequenciesObserved = [];
 
-            for(let j = 0; j<inputs.length; j++){
-                let t = j / this.sampleRate;
-                sinSum += Math.sin(f*t)*inputs[j];
-                cosSum += Math.cos(f*t)*inputs[j];
+        for(let i = 0; i<this.frequencyTargets.length; i++){
+            let maxAmplitude = 0;
+            let maxFreq = -1;
+            let frequency = this.frequencyTargets[i];
+
+            for(let f = frequency - this.frequencyBand; f <= frequency + this.frequencyBand; f++){
+                let sinSum = 0;
+                let cosSum = 0;
+
+                for(let j = 0; j<inputs.length; j++){
+                    let t = j / this.sampleRate;
+                    sinSum += Math.sin(f*t)*inputs[j];
+                    cosSum += Math.cos(f*t)*inputs[j];
+                }
+
+                let amplitude = Math.sqrt(sinSum*sinSum + cosSum*cosSum);
+                if(amplitude > maxAmplitude) {
+                    maxAmplitude = amplitude;
+                    maxFreq = f;
+                }
             }
+            
+            frequenciesObserved.push(maxFreq);
+        }   
 
-            let amplitude = Math.sqrt(sinSum*sinSum + cosSum*cosSum);
-            if(amplitude > maxAmplitude) {
-                maxAmplitude = amplitude;
-                maxFreq = f;
-            }
+        let calculatedSpeeds = [];
+        
+
+        for(let i = 0; i<this.frequencyTargets.length; i++){
+            let v = speedOfSound * (frequenciesObserved[i] / this.frequencyTargets[i] - 1);
+            calculatedSpeeds.push(v);
         }
 
         this.port.postMessage({
             type:'dopplerData',
-            maxFrequency: maxFreq
+            frequenciesObserved,
+            calculatedSpeeds
         });
         
 
